764c17419876a00dee10074677ccd96879f62d2b
Alaskarli <nazrinna@code.edu.az>
2023-01-25 18:01:53 -0800

    Class

---
+++ test/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Animal
+    {
+        public string name;
+        public string breed;
+        public string color;
+        public int age;
+
+
+
+
+        public Animal(string name,string breed,int age)
+        { 
+            this.name = name;
+            this.breed = breed;
+            this.age = age;
+
+        }
+
+
+        public string GetDetails()
+        {
+            return $"{name} {breed} {color} {age}";
+            
+        }
+
+
+
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace test
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Car car1= new Car();
+            //car1.colour = "red";
+            //car1.name = "BMW";
+            //car1.horsePower = 123;
+            //car1.highspeed = 324;
+            //Console.WriteLine(car1.GetDetails());
+
+
+
+
+            Animal Animal1 = new Animal("dog", "terrier", 4);
+         
+            Console.WriteLine(Animal1.GetDetails());
+
+            Building Building1= new Building("happy homes",200,600);
+            Console.WriteLine(Building1.Volume());
+
+
+
+
+            Console.WriteLine(Building1.GetDetails());
+            //Console.WriteLine(Building2.GetDetails());
+        }
+       
+
+        
+
+    }
+
+
+
+
+
+
+
+
+}
